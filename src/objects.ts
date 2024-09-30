5292694c73994956c50395ad917fe7505a42ca2e
Axel <axelarl@udel.edu>
2024-09-30 05:47:58 -0400

    Fixed toShortForm

--- src/objects.ts
+++ src/objects.ts
@@ -57,7 +57,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return question.id.toString() + ": " + question.name.slice(0, 9);
+    return `${question.id}: ${question.name.slice(0, 10)}`;
 }
 
 /**
